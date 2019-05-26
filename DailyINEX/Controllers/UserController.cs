3ba26a5e763f1b556fd663a665f9ca9655f54fa9
rimon28 <rimon.kumar.cse@gmail.com>
2019-05-26 13:05:21 +0600

    first commit

---
+++ DailyINEX/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataLibrary.BusinessLogic;
+using DataLibrary.BusinessModel;
+
+namespace DailyINEX.Controllers
+{
+    public class UserController : Controller
+    {
+        //Registraion  
+
+        public ActionResult Registration()
+        {
+            ViewBag.CountryList = CountryProcessor.LoadCountry();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(Company data )
+        {
+            bool isSaved = CompanyProcessor.SaveCompany(data);
+            if (isSaved)
+            {
+                ViewBag.Message = "Registraion Successfull";
+
+            }
+            
+            return View();
+        }
+
+        //Login
+    }
+}
